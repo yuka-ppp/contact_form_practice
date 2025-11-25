11008f5b34b934967cb8fe0b966b6dca7a8cdf13
yuka <yukarinman.0413.ren@gmail.com>
2025-11-26 03:43:45 +0900

    Add project files and gitignore

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->numberBetween(1, 3),
+            'email' => $this->faker->safeEmail(),
+            'tel' => $this->faker->numberBetween(10000,99999),
+            'address' => $this->faker->address(),
+            'building' => $this->faker->optional()->secondaryAddress(),
+            'detail' => $this->faker->realText(120)
+        ];
+    }
+}
